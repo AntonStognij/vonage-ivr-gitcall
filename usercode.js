3b377f8fabebdec290f80ee8f7929477a9503858
AntonStognij <98051415+AntonStognij@users.noreply.github.com>
2026-09-10 16:47:45 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,7 +1,33 @@
-module.exports = (data) => {
-    data.env = {
-        test: process.env.TEST_VARIABLE
-    };
+const { Vonage } = require("@vonage/server-sdk");
+
+module.exports = async (data) => {
+    const vonage = new Vonage({
+        applicationId: data.vonageApplicationId,
+        privateKey: data.vonagePrivateKey
+    });
+
+    const digits = String(data.code).split("").join(" ");
+
+    const call = await vonage.voice.createOutboundCall({
+        to: [
+            {
+                type: "phone",
+                number: data.phone
+            }
+        ],
+        from: {
+            type: "phone",
+            number: data.vonageFromNumber
+        },
+        ncco: [
+            {
+                action: "talk",
+                text: `Ваш код ${digits}`
+            }
+        ]
+    });
+
+    data.call = call;
 
     return data;
 };

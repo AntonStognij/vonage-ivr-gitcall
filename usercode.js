a56df682563f79fb8674832f89fcba2be92a892f
AntonStognij <98051415+AntonStognij@users.noreply.github.com>
2026-09-16 17:53:04 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,36 +1,36 @@
-const { Vonage } = require("@vonage/server-sdk");
+// const { Vonage } = require("@vonage/server-sdk");
 
-module.exports = async (data) => {
-    const vonage = new Vonage({
-        applicationId: data.vonageApplicationId,
-        privateKey: data.vonagePrivateKey
-    });
+// module.exports = async (data) => {
+//     const vonage = new Vonage({
+//         applicationId: data.vonageApplicationId,
+//         privateKey: data.vonagePrivateKey
+//     });
 
-    const digits = String(data.code).split("").join(" ");
+//     const digits = String(data.code).split("").join(" ");
 
-    const call = await vonage.voice.createOutboundCall({
-        to: [
-            {
-                type: "phone",
-                number: data.phone
-            }
-        ],
-        from: {
-            type: "phone",
-            number: data.vonageFromNumber
-        },
-        ncco: [
-            {
-                action: "talk",
-                text: `Ваш код ${digits}`,
-                provider: "google",
-                "providerOptions": {
-                "name": "uk-UA-Chirp3-HD-Achernar",
-                "language_code": "uk-UA"
-      }
-            }
-        ]
-    });
+//     const call = await vonage.voice.createOutboundCall({
+//         to: [
+//             {
+//                 type: "phone",
+//                 number: data.phone
+//             }
+//         ],
+//         from: {
+//             type: "phone",
+//             number: data.vonageFromNumber
+//         },
+//         ncco: [
+//             {
+//                 action: "talk",
+//                 text: `Ваш код ${digits}`,
+//                 provider: "google",
+//                 "providerOptions": {
+//                 "name": "uk-UA-Chirp3-HD-Achernar",
+//                 "language_code": "uk-UA"
+//       }
+//             }
+//         ]
+//     });
 
     data.call = call;
 
